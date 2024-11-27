2be452f325d47ec078490a4701debf6e12f8702f
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-11-27 16:06:05 -0300

    update tables creation

--- create_tables.sql
+++ create_tables.sql
@@ -1,51 +1,46 @@
-CREATE TABLE compradores (
+CREATE TABLE admin (
     dni INT PRIMARY KEY, 
     nombre VARCHAR(50)
 );
 
-CREATE TABLE vendedores (
+CREATE TABLE comprador (
     dni INT PRIMARY KEY, 
     nombre VARCHAR(50)
 );
 
-CREATE TABLE publicaciones (
+CREATE TABLE vendedor (
+    dni INT PRIMARY KEY, 
+    nombre VARCHAR(50)
+);
+
+CREATE TABLE publicacion (
     vendedor INT,
     producto VARCHAR(200), 
     precio DECIMAL(10, 2) CHECK (precio > 1000), 
     disponible BOOLEAN,
     PRIMARY KEY (vendedor, producto), 
-    FOREIGN KEY (vendedor) REFERENCES vendedores(dni)
-);
+    FOREIGN KEY (vendedor) REFERENCES vendedor(dni)
+); 
 
-CREATE TABLE caracteristicas (
+CREATE TABLE caracteristica (
     caracteristica VARCHAR(200),
     producto VARCHAR(200),
     vendedor INT,
-    valor VARCHAR(200),
     PRIMARY KEY (caracteristica, producto, vendedor),
-    FOREIGN KEY (producto, vendedor) REFERENCES publicaciones(producto, vendedor)
+    FOREIGN KEY (producto, vendedor) REFERENCES publicacion(producto, vendedor)
 );
 
-CREATE TABLE categorias (
-    categoria VARCHAR(200),
-    producto VARCHAR(200),
-    vendedor INT,
-    PRIMARY KEY (categoria, producto, vendedor),
-    FOREIGN KEY (producto, vendedor) REFERENCES publicaciones(producto, vendedor),
-    FOREIGN KEY (vendedor) REFERENCES vendedores(dni)
-);
-
-CREATE TABLE compras (
+CREATE TABLE compra (
     vendedor INT,
     producto VARCHAR(200),
     comprador INT,
     estado_envio VARCHAR(50),
     estado_compra VARCHAR(50),
     fecha_compra DATE,
-    forma_pago VARCHAR(50),
+    forma_de_pago VARCHAR(50),
     monto_total DECIMAL(10, 2),
     PRIMARY KEY (comprador, vendedor, producto, fecha_compra),
-    FOREIGN KEY (vendedor, producto) REFERENCES publicaciones(vendedor, producto),
-    FOREIGN KEY (vendedor) REFERENCES vendedores(dni),
-    FOREIGN KEY (comprador) REFERENCES compradores(dni)
+    FOREIGN KEY (vendedor, producto) REFERENCES publicacion(vendedor, producto),
+    FOREIGN KEY (vendedor) REFERENCES vendedor(dni),
+    FOREIGN KEY (comprador) REFERENCES comprador(dni)
 );
