f41c92ba8b625548288394eb886f99b3c1e46ffe
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-11-27 16:58:12 -0300

    better sintax grant roles

--- create_roles.sql
+++ create_roles.sql
@@ -1,25 +1,16 @@
+/*** Rol admin ***/
 CREATE ROLE admin LOGIN PASSWORD 'admin_pass';
 GRANT ALL PRIVILEGES ON DATABASE web_compra_venta TO admin;
 
-
-
+/*** Rol vendedor ***/
 CREATE ROLE vendedor LOGIN PASSWORD 'vendedor_pass';
+GRANT 
+    SELECT, UPDATE, INSERT ON TABLE publicacion TO vendedor,
+    SELECT, UPDATE, INSERT, DELETE ON TABLE caracteristica TO vendedor,
+    SELECT ON TABLE compra TO vendedor;
 
-GRANT SELECT ON TABLE publicacion TO vendedor;
-GRANT UPDATE ON TABLE publicacion TO vendedor;
-GRANT INSERT ON TABLE publicacion TO vendedor;
-
-GRANT SELECT ON TABLE caracteristica TO vendedor;
-GRANT UPDATE ON TABLE caracteristica TO vendedor;
-GRANT INSERT ON TABLE caracteristica TO vendedor;
-GRANT DELETE ON TABLE caracteristica TO vendedor;
-
-GRANT SELECT ON TABLE compra TO vendedor;
-
-
-
+/*** Rol comprador ***/
 CREATE ROLE comprador LOGIN PASSWORD 'comprador_pass';
-
-GRANT SELECT ON TABLE compra TO comprador;
-GRANT UPDATE ON TABLE compra TO comprador;
-GRANT INSERT ON TABLE compra TO comprador;
+GRANT 
+    SELECT, UPDATE, INSERT ON TABLE compra TO comprador,
+    SELECT ON TABLE publicacion TO comprador;
