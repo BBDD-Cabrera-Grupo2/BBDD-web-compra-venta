ae359db13e3aa2ce0853c1c83d3fb6de2d89e808
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-11-27 19:47:43 -0300

    ver publicaciones disponibles +

--- extra_queries.sql
+++ extra_queries.sql
@@ -4,3 +4,8 @@ DROP CONSTRAINT publicacion_precio_min;
 
 ALTER TABLE publicacion
 ADD CONSTRAINT publicacion_precio_min CHECK (precio > 900 OR NOT disponible);
+
+-- Publicaciones disponibles para la venta
+SELECT *
+FROM publicacion
+WHERE disponible == TRUE;
