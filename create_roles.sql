1a67985793009b68fe39a4b3b9bcc907bea224c4
Patricio Ibar <patricioibar10@gmail.com>
2024-11-27 17:21:54 -0300

    cambiando comentarios

--- create_roles.sql
+++ create_roles.sql
@@ -1,15 +1,15 @@
-/*** Rol admin ***/
+-- Rol admin
 CREATE ROLE admin LOGIN PASSWORD 'admin_pass';
 GRANT ALL PRIVILEGES ON DATABASE web_compra_venta TO admin;
 
-/*** Rol vendedor ***/
+-- Rol vendedor
 CREATE ROLE vendedor LOGIN PASSWORD 'vendedor_pass';
 GRANT 
     SELECT, UPDATE, INSERT ON TABLE publicacion TO vendedor,
     SELECT, UPDATE, INSERT, DELETE ON TABLE caracteristica TO vendedor,
     SELECT ON TABLE compra TO vendedor;
 
-/*** Rol comprador ***/
+-- Rol comprador
 CREATE ROLE comprador LOGIN PASSWORD 'comprador_pass';
 GRANT 
     SELECT, UPDATE, INSERT ON TABLE compra TO comprador,
--- queries_varias.sql
+++ queries_varias.sql
@@ -1,31 +1,31 @@
-/* Listar todos los productos de la plataforma */
+-- Listar todos los productos de la plataforma
 SELECT DISTINCT producto FROM publicacion; 
 
-/* Listar productos de la plataforma por categoría */
+-- Listar productos de la plataforma por categoría
 SELECT * 
 FROM publicacion p
 WHERE categoria = 'Tecnología';
 
-/* Crear una publicación para vender */
+-- Crear una publicación para vender
 INSERT INTO publicacion (vendedor, producto, categoria, precio, disponible)
 VALUES (98765432, 'Bicicleta', 'Deportes', 154000, TRUE);
 
-/* Actualizar una publicación de venta */
+-- Actualizar una publicación de venta
 UPDATE publicacion
 SET precio = 160000
 WHERE vendedor = 98765432 AND producto = 'Bicicleta';
 
-/* Realizar una compra */
+-- Realizar una compra
 INSERT INTO compras (vendedor, producto, comprador, estado_envio, estado_compra, fecha_compra, forma_pago)
 VALUES (98765432, 'Bicicleta', 11111111, 'EN PREPARACION', 'COMPRADO', '2024-11-25', 'EFECTIVO');
 
-/* Guardar un envío */
+-- Guardar un envío
 UPDATE compras
 SET estado_envio = 'RECIBIDO'
 WHERE vendedor = 98765432 AND producto = 'Bicicleta'
 AND comprador = 11111111 AND fecha_compra = '2024-11-25'; 
 
-/* Cancelar una compra */
+-- Cancelar una compra
 UPDATE compras
 SET estado_compra = 'CANCELADO', estado_envio = 'CANCELADO'
 WHERE vendedor = 98765432 
@@ -33,15 +33,15 @@ AND producto = 'Bicicleta'
 AND comprador = 11111111 
 AND fecha_compra = '2024-11-25';
 
-/* Cancelar envío */
+-- Cancelar envío
 UPDATE compras
 SET estado_envio = 'CANCELADO'
 WHERE vendedor = 98765432 AND producto = 'Bicicleta'
 AND comprador = 11111111 AND fecha_compra = '2024-11-25';
 
-/* Ver reputaciones */
+-- Ver reputaciones
 
-/*** Compradores ***/
+-- Compradores
 WITH compras_ultimos_30_dias AS (
     SELECT comprador, SUM(monto_total) AS total_gastado
     FROM compras
@@ -53,7 +53,7 @@ SELECT c.dni, c.nombre, co.total_gastado AS reputacion
 FROM compradores c
 JOIN compras_ultimos_30_dias co ON c.dni = co.comprador;
 
-/*** Vendedores ***/
+-- Vendedores
 WITH ventas_ultimos_30_dias AS (
     SELECT vendedor, SUM(monto_total) AS total_cobrado
     FROM compras
@@ -65,14 +65,14 @@ SELECT v.dni, v.nombre, ve.total_cobrado AS reputacion
 FROM vendedores v
 JOIN ventas_ultimos_30_dias ve ON v.dni = ve.vendedor;
 
-/* Contabilizar la cantidad de ventas de plataforma para un rango de fechas */
+-- Contabilizar la cantidad de ventas de plataforma para un rango de fechas
 SELECT COUNT(*) AS cantidad_de_ventas 
 FROM compras
 WHERE fecha_compra < '2024-11-28' 
 AND fecha_compra > '2024-11-01' 
 AND estado_compra = 'COMPRADO';
 
-/* Contabilizar el valor total de ventas de la plataforma para un rango de fechas */
+-- Contabilizar el valor total de ventas de la plataforma para un rango de fechas
 SELECT SUM(monto_total) AS valor_total_ventas
 FROM compras
 WHERE fecha_compra < '2024-11-28' 
